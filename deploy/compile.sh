3780f430baa168c7f9ce0149b413c8de6f974f45
Lingzhe Zhao <zhaolingzhe@westlake.edu.cn>
2024-03-27 20:25:02 +0800

    Fix docker compose build with ros/noetic

--- deploy/compile.sh
+++ deploy/compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-source /opt/ros/melodic/setup.bash
+source /opt/ros/noetic/setup.bash
 catkin build maplab maplab_console maplab_node maplab_server_node
--- deploy/set_env.sh
+++ deploy/set_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-. /opt/ros/melodic/setup.bash
+. /opt/ros/noetic/setup.bash
 . /usr/home/ws/devel/setup.bash
 roscore > /dev/null &
 exec "$@"
